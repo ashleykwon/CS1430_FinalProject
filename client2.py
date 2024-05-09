bdb4c8449c53bed6417dbcd9269c41485cf2fc8e
Ashley Kwon <hk216@duke.edu>
2024-05-09 17:03:27 -0400

    Debugging instructions modified

--- client2.py
+++ client2.py
@@ -16,22 +16,23 @@ def send_video():
 
     # Start video capture using the two webcams
     cap0 = cv2.VideoCapture(0)
-    # cap1 = cv2.VideoCapture(1)
+    cap1 = cv2.VideoCapture(1) # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
 
     # get frame size and generate client ID
     ret, frame = cap0.read()
-    # ret1, frame1 = cap1.read()
+    ret1, frame1 = cap1.read() # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
     w, h, c = frame.shape
-    # w1, h1, c1 = frame1.shape
+    w1, h1, c1 = frame1.shape # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
     data = frame.flatten().tobytes() # frame from camera 1
-    # data2 = frame1.flatten().tobytes() # frame from camera 2
-    # size = len(data)+len(data2)
-    size = len(data) # for debugging purposes only 
+    data2 = frame1.flatten().tobytes() # frame from camera 2 # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+    size = len(data)+len(data2) # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+    # size = len(data) # UNCOMMENT THIS FOR DEBUGGING
     clientID = 2
 
     # Send the videos captured from two webcams to the server
-    # client_socket.sendall(struct.pack("Q", w+w1) + struct.pack("Q", h+h1) + struct.pack("Q", c+c1) + struct.pack("Q", size))
-    client_socket.sendall(struct.pack("Q", w) + struct.pack("Q", h) + struct.pack("Q", c) + struct.pack("Q", size) + struct.pack("Q", clientID)) 
+    client_socket.sendall(struct.pack("Q", w+w1) + struct.pack("Q", h+h1) + struct.pack("Q", c+c1) + struct.pack("Q", size)) # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+    # client_socket.sendall(struct.pack("Q", w) + struct.pack("Q", h) + struct.pack("Q", c) + struct.pack("Q", size) + struct.pack("Q", clientID)) # UNCOMMENT THIS FOR DEBUGGING
+    
     # send video
     try:
         while True:
@@ -39,13 +40,13 @@ def send_video():
             # ret1, frame1 = cap1.read()
             if not ret:
                 break
-            # joined_frame = np.concatenate((frame, frame1))
-            # joined_frame_data = joined_frame.flatten().tobytes()
-            # client_socket.sendall(joined_frame_data)
-            client_socket.sendall(frame) # for debugging purposes only. Use the line above instead 
+            joined_frame = np.concatenate((frame, frame1)) # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+            joined_frame_data = joined_frame.flatten().tobytes() # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+            client_socket.sendall(joined_frame_data) # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
+            # client_socket.sendall(frame) # UNCOMMENT THIS FOR DEBUGGING
     finally:
         cap0.release()
-        # cap1.release()
+        cap1.release() # COMMENT THIS OUT WHEN ONLY USING ONE CAMERA
         client_socket.close()
 
 
