513056061dc969533e72c946ccd94cb2a0045063
apoorvkh <mail@apoorvkh.com>
2024-05-11 17:26:31 -0400

    fix typo in intrinsics calculation

--- src/intrinsics.py
+++ src/intrinsics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def get_intrinsic_matrix(fov_x, fov_y, W, H):
     f_x = (W / 2) / np.tan(np.radians(fov_x / 2))
-    f_y = (W / 2) / np.tan(np.radians(fov_x / 2))
+    f_y = (W / 2) / np.tan(np.radians(fov_y / 2))
     s = 0.0
     c_x = W / 2
     c_y = H / 2
