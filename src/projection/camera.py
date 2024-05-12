2ecc5c51ec4eab0353908d88c70b2b530b4ef1e6
ashleykwon <hojung_kwon@brown.edu>
2024-05-12 16:29:11 -0400

    debugging client calibrate

--- src/projection/camera.py
+++ src/projection/camera.py
@@ -20,33 +20,38 @@ def stereo_calibration(K_l, K_r, chessboard_images, chess_box_size_mm):
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((10 * 7, 3), np.float32)
+    objp[:, :2] = np.mgrid[:7, :10].T.reshape(-1, 2)
+    # objp = objp * chess_box_size_mm
+
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpointsL = []  # 2d points in left image plane.
     imgpointsR = []  # 2d points in right image plane.
 
     for imgL, imgR in tqdm(chessboard_images):
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        objp = np.zeros((10 * 7, 3), np.float32)
-        objp[:, :2] = np.mgrid[:10, :7].T.reshape(-1, 2)
-        # objp = objp * chess_box_size_mm
-        objpoints.append(objp)
+        retL, cornersL = cv2.findChessboardCorners(image=imgL, patternSize=(7, 10))
+        retR, cornersR = cv2.findChessboardCorners(image=imgR, patternSize=(7, 10))
 
-        cornersL = cv2.findChessboardCorners(image=imgR, patternSize=(10, 7))[1]
-        imgL_gray = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-        cornersL = cv2.cornerSubPix(imgL_gray, cornersL, (11, 11), (-1, -1), criteria)
-        imgpointsL.append(cornersL)
+        if retL and retR:
+            objpoints.append(objp)
 
-        cornersR = cv2.findChessboardCorners(image=imgL, patternSize=(10, 7))[1]
-        imgR_gray = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
-        cornersR = cv2.cornerSubPix(imgR_gray, cornersR, (11, 11), (-1, -1), criteria)
-        imgpointsR.append(cornersR)
+            imgL_gray = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+            cornersL = cv2.cornerSubPix(imgL_gray, cornersL, (11, 11), (-1, -1), criteria)
+            imgpointsL.append(cornersL)
+
+            imgR_gray = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+            cornersR = cv2.cornerSubPix(imgR_gray, cornersR, (11, 11), (-1, -1), criteria)
+            imgpointsR.append(cornersR)
 
     K_l_, distL = cv2.calibrateCamera(
         objectPoints=objpoints,
         imagePoints=imgpointsL,
-        imageSize=imgL_gray.shape,
-        cameraMatrix=K_l,
+        imageSize=chessboard_images[0][0].shape[:2][::-1],
+        # cameraMatrix=None,
+        # distCoeffs=None,
+        cameraMatrix=K_l.copy(),
         distCoeffs=None,
         flags=(cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT)
     )[1:3]
@@ -54,10 +59,12 @@ def stereo_calibration(K_l, K_r, chessboard_images, chess_box_size_mm):
     K_r_, distR = cv2.calibrateCamera(
         objectPoints=objpoints,
         imagePoints=imgpointsR,
-        imageSize=imgL_gray.shape,
-        cameraMatrix=K_r,
+        imageSize=chessboard_images[0][1].shape[:2][::-1],
+        cameraMatrix=None,
         distCoeffs=None,
-        flags=(cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT)
+        # cameraMatrix=K_r,
+        # distCoeffs=None,
+        # flags=(cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT)
     )[1:3]
 
     print("K_l")
