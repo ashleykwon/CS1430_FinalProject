56282edaf58834c3dddd93197c45f3376cbb2d91
apoorvkh <mail@apoorvkh.com>
2024-05-12 15:05:56 -0400

    updates to calib

--- src/projection/camera.py
+++ src/projection/camera.py
@@ -1,7 +1,7 @@
 import io
 import json
 import numpy as np
-import cv2 as cv
+import cv2
 import os
 
 from tqdm import tqdm
@@ -17,82 +17,92 @@ def get_intrinsic_matrix(fov_x, fov_y, W, H):
 
 
 def stereo_calibration(K_l, K_r, chessboard_images, chess_box_size_mm):
-    if not os.path.exists("1_corners"):
-        os.makedirs("1_corners")
-    if not os.path.exists("2_corners"):
-        os.makedirs("2_corners")
-
     # termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((7 * 10, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:7, 0:10].T.reshape(-1, 2)
-    objp = objp * chess_box_size_mm
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpointsL = []  # 2d points in left image plane.
     imgpointsR = []  # 2d points in right image plane.
 
-    for i in tqdm(range(len(chessboard_images))):
-        imgL = cv.imread("1/%d.jpg" % i)
-        imgR = cv.imread("2/%d.jpg" % i)
-        imgL_gray = cv.imread("1/%d.jpg" % i, 0)
-        imgR_gray = cv.imread("2/%d.jpg" % i, 0)
+    for imgL, imgR in tqdm(chessboard_images):
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((10 * 7, 3), np.float32)
+        objp[:, :2] = np.mgrid[:10, :7].T.reshape(-1, 2)
+        # objp = objp * chess_box_size_mm
+        objpoints.append(objp)
 
-        outputL = imgL.copy()
-        outputR = imgR.copy()
+        cornersL = cv2.findChessboardCorners(image=imgR, patternSize=(10, 7))[1]
+        imgL_gray = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+        cornersL = cv2.cornerSubPix(imgL_gray, cornersL, (11, 11), (-1, -1), criteria)
+        imgpointsL.append(cornersL)
 
-        retR, cornersR = cv.findChessboardCorners(outputR, (7, 10), None)
-        retL, cornersL = cv.findChessboardCorners(outputL, (7, 10), None)
+        cornersR = cv2.findChessboardCorners(image=imgL, patternSize=(10, 7))[1]
+        imgR_gray = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+        cornersR = cv2.cornerSubPix(imgR_gray, cornersR, (11, 11), (-1, -1), criteria)
+        imgpointsR.append(cornersR)
 
-        if retR and retL:
-            objpoints.append(objp)
-            cv.cornerSubPix(imgR_gray, cornersR, (11, 11), (-1, -1), criteria)
-            cv.cornerSubPix(imgL_gray, cornersL, (11, 11), (-1, -1), criteria)
-            cv.drawChessboardCorners(outputR, (7, 10), cornersR, retR)
-            cv.drawChessboardCorners(outputL, (7, 10), cornersL, retL)
-            cv.imwrite("1_corners/%d.jpg" % i, outputL)
-            cv.imwrite("2_corners/%d.jpg" % i, outputR)
+    K_l_, distL = cv2.calibrateCamera(
+        objectPoints=objpoints,
+        imagePoints=imgpointsL,
+        imageSize=imgL_gray.shape,
+        cameraMatrix=K_l,
+        distCoeffs=None,
+        flags=(cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT)
+    )[1:3]
 
-            imgpointsL.append(cornersL)
-            imgpointsR.append(cornersR)
+    K_r_, distR = cv2.calibrateCamera(
+        objectPoints=objpoints,
+        imagePoints=imgpointsR,
+        imageSize=imgL_gray.shape,
+        cameraMatrix=K_r,
+        distCoeffs=None,
+        flags=(cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT)
+    )[1:3]
 
-    # Calibrating left camera
-    retL, mtxL, distL, rvecsL, tvecsL = cv.calibrateCamera(
-        objpoints, imgpointsL, imgL_gray.shape[::-1], None, None
-    )
+    print("K_l")
+    print(K_l)
+    print("K_l_")
+    print(K_l_)
 
-    # Calibrating right camera
-    retR, mtxR, distR, rvecsR, tvecsR = cv.calibrateCamera(
-        objpoints, imgpointsR, imgR_gray.shape[::-1], None, None
-    )
+    print("K_r")
+    print(K_r)
+    print("K_r_")
+    print(K_r_)
 
-    flags = 0
-    flags |= cv.CALIB_FIX_INTRINSIC
-    # Here we fix the intrinsic camara matrixes so that only Rot, Trns, Emat and Fmat are calculated.
-    # Hence intrinsic parameters are the same
+    print("distL")
+    print(distL)
+    print("distR")
+    print(distR)
 
-    criteria_stereo = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    criteria_stereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # This step is performed to transformation between the two cameras and calculate Essential and Fundamental matrix
-    retS, mtxL, distL, mtxR, distR, Rot, Trns, Emat, Fmat = cv.stereoCalibrate(
-        objpoints,
-        imgpointsL,
-        imgpointsR,
-        K_l,
-        distL,
-        K_r,
-        distR,
-        imgL_gray.shape[::-1],
-        criteria_stereo,
-        flags,
+    retS, mtxL, distL, mtxR, distR, Rot, Trns, Emat, Fmat = cv2.stereoCalibrate(
+        objectPoints=objpoints,
+        imagePoints1=imgpointsL,
+        imagePoints2=imgpointsR,
+        cameraMatrix1=K_l,
+        distCoeffs1=distL,
+        cameraMatrix2=K_r,
+        distCoeffs2=distR,
+        imageSize=chessboard_images[0][0].shape[:2],
+        flags=cv2.CALIB_FIX_INTRINSIC,
+        criteria=criteria_stereo,
     )
-    
+
     R_l = Rot
     t_l = Trns
     R_r = np.identity(3)
     t_r = np.zeros(3)
-    
+
+    print("R_l")
+    print(R_l)
+    print("t_l")
+    print(t_l)
+    print("R_r")
+    print(R_r)
+    print("t_r")
+    print(t_r)
+
     return R_l, t_l, R_r, t_r
